5c1d7099e7215ab965e8be9c85460a1baa72b166
Atul Rauthan <atulrauthan7@gmail.com>
2021-03-08 14:34:40 +0530

    added mongoose data model

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 const PORT = 3000
+const Store = require('./api/models/store')
 
 mongoose.connect('mongodb+srv://atul_pwj:Cupertino@10@pwjatul.lqeaq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
 
@@ -13,7 +14,19 @@ app.get('/', (req, res) => {
 
 app.post('/api/stores', (req, res) => {
     let dbStores = req.body
-    console.log(dbStores)
+    // var store = new Store({
+    //     storeName: "Test",
+    //     phoneNumber: "1234567890",
+    //     location: {
+    //         type: 'Point',
+    //         coordinates: [
+    //             -118.376354,
+    //             34.063584
+    //         ]
+    //     }
+    // })
+    // store.save()
+    res.send('Data saved!')
 })
 
 app.listen(PORT, () => {
